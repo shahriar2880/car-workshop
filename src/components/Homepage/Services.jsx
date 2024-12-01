04c9d00ae78f2ce80900c7686486025353bdd1a1
shahriar2880 <shahriarreza436@gmail.com>
2024-12-01 18:48:04 +0600

    Services component in home page

---
+++ src/components/Homepage/Services.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <div>
+        <h3>Our Services</h3>
+        <h2>Our Service Area</h2>
+        <p>First appearing in the early 1900s,[2] many filling stations offered vehicle repair services as part of their full service operation. This once popular trend has declined significantly over the years as many locations found it more profitable to exchange vehicle service bays for grocery isles, which ultimately led to the emergence of the quick oil change industry.</p>
+      </div>
+      <div className='container mx-auto mt-12'>
+        Service Cards
+      </div>
+    </div>
+  )
+}
+
+export default Services
