476c2a2fb90a4a098d2af80339678ffc342bb4c3
shahriar2880 <shahriarreza436@gmail.com>
2024-12-02 00:33:24 +0600

    Complete About Section

--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-slate-900 mt-5 mb-3'>
+    <div className='text-slate-900 mt-5 mb-20'>
       <div className='container mx-auto'>
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-2'>
+      <div className='grid grid-cols-1 lg:grid-cols-2'>
       <div className="relative">
         <Image
           src="/assets/images/about_us/person.jpg"
           alt="logo of author"
-          width={800}
+          width={600}
           height={50}
           className="object-cover" // Maintain aspect ratio
         />
@@ -19,18 +19,18 @@ const About = () => {
           alt="logo of author"
           width={350}
           height={80}
-          className="absolute bottom-3 right-4 border-10 border-white object-cover"
+          className="absolute -bottom-12 right-12 object-cover"
+          style={{ border: '10px solid white' }}
         />
       </div>
-        <div className='full'>
-            <h1 className='text-red-700'>About Us</h1>
-            <h1 className='text-5xl'>We are qualified & experienced in this field</h1>
-            <p>
-              Our mission is to achieve customer satisfaction by providing quality services with competitive prices, commitment and on-time delivery.
-              Our vision is to be known as the reliable, innovative and user friendly web & software service provider in the IT industry of Bangladesh at the same time creating more employment opportunities for the youth. We believe in working with accuracy and quality.
-            </p>
-            <button className='btn btn-success'>Get More Info</button>
-        </div>
+      <div className="space-y-10 ">
+        <h1 className="text-red-700 font-bold">About Us</h1>
+        <h2 className="text-5xl font-bold">We are qualified & experienced in this field</h2>
+        <p>
+          Our mission is to achieve customer satisfaction by providing quality services with competitive prices, commitment, and on-time delivery. Our vision is to be known as the reliable, innovative, and user-friendly web & software service provider in the IT industry of Bangladesh, while also creating more employment opportunities for the youth. We believe in working with accuracy and quality.
+        </p>
+          <button className="btn btn-success">Get More Info</button>
+    </div>
       </div>
       </div>
     </div>
--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -5,10 +5,10 @@ const Services = () => {
     <div className='text-slate-800 min-h-screen'>
       <div className='text-center container mx-auto'>
         <h3 className=''>Our Services</h3>
-        <h2>Our Service Area</h2>
+        <h2 className='text-4xl font-bold'>Our Service Area</h2>
         <p>First appearing in the early 1900s,[2] many filling stations offered vehicle repair services as part of their full service operation. This once popular trend has declined significantly over the years as many locations found it more profitable to exchange vehicle service bays for grocery isles, which ultimately led to the emergence of the quick oil change industry.</p>
       </div>
-      <div className='container mx-auto mt-12'>
+      <div className='container mx-auto mt-12 font-bold text-red-700'>
         Service Cards
       </div>
     </div>
