a5211cabefb2fca189fa8e967ab36b93b431864b
shahriar2880 <shahriarreza436@gmail.com>
2024-12-03 23:00:57 +0600

    Showed ServicesCard in service area

--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ServiceCard from '../card/ServiceCard'
+import {services} from '../../lib/services'
 
 const Services = () => {
+    console.log(services);
   return (
     <div className='text-slate-800 min-h-screen'>
       <div className='text-center container mx-auto'>
@@ -9,8 +11,12 @@ const Services = () => {
         <h2 className='text-4xl font-bold'>Our Service Area</h2>
         <p>First appearing in the early 1900s,[2] many filling stations offered vehicle repair services as part of their full service operation. This once popular trend has declined significantly over the years as many locations found it more profitable to exchange vehicle service bays for grocery isles, which ultimately led to the emergence of the quick oil change industry.</p>
       </div>
-      <div className='container mx-auto mt-12 font-bold text-red-700'>
-        <ServiceCard/>
+      <div className='container mx-auto mt-12 font-bold text-red-700 grid grid-cols-1 lg:grid-cols-3 gap-6'>
+        {
+            services.map((service) =>(
+                <ServiceCard service={service} key={service._id}/>
+            ))
+        }
       </div>
     </div>
   )
