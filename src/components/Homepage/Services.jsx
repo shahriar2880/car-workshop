d374900df954eb0e25ce04dfafd5ef12a495d573
shahriar2880 <shahriarreza436@gmail.com>
2024-12-05 23:33:06 +0600

    Create Log in and Signup folder

--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
       <div className='text-center container mx-auto'>
         <h3 className=''>Our Services</h3>
         <h2 className='text-4xl font-bold'>Our Service Area</h2>
-        <p>First appearing in the early 1900s,[2] many filling stations offered vehicle repair services as part of their full service operation. This once popular trend has declined significantly over the years as many locations found it more profitable to exchange vehicle service bays for grocery isles, which ultimately led to the emergence of the quick oil change industry.</p>
+        <p>First appearing early 1900s,[2] many filling stations offered vehicle repair services as part of their full service operation. This once popular trend has declined significantly over the years as many locations found it more profitable to exchange vehicle service bays for grocery isles, which ultimately led to the emergence of the quick oil change industry.</p>
       </div>
       <div className='container mx-auto mt-12 font-bold text-red-700 grid grid-cols-1 lg:grid-cols-3 gap-6 '>
         {
