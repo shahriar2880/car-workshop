58e183924e0565a678df60481d9529221ebc40ff
shahriar2880 <shahriarreza436@gmail.com>
2024-12-07 02:29:49 +0600

    Complete login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
 import { FcGoogle } from "react-icons/fc";
@@ -5,24 +6,26 @@ import { BsGithub } from "react-icons/bs";
 import { FaFacebook } from "react-icons/fa";
 import Link from 'next/link';
 
-
-
 const page = () => {
+  const handleLogin = async () =>{
+
+  }
   return (
     <div className='container px-24 mx-auto py-24'>
-        
       <div className='grid grid-cols-2 gap-12 items-center'>
         <div>
             <Image src='/assets/images/login/login.svg' height="540" width="540" alt='login Image'/>
         </div>
         <div className='border-2 p-12'>
         <h6 className='text-3xl font-semibold text-center mb-12'>Log In</h6>
+            <form onSubmit={handleLogin} action=''>
             <label htmlFor='email'>Email</label><br/>
             <input type="text" name='email' placeholder="Your Email" className="mt-3 input input-bordered w-full" /> <br/><br/>
             <label htmlFor='email'>Password</label><br/>
             <input type="text" name='password' placeholder="Your Password" className="mt-3 input input-bordered w-full" />
             <br/><br/>
-            <button className='w-full btn btn-primary'>Log In</button>
+            <button type='submit' className='w-full btn btn-primary'>Log In</button>
+            </form>
             <div>
                 <h6 className='my-12 text-center'>Or Sign In With</h6>
                 <div className='flex items-center justify-center space-x-2'>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,10 +1,14 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+import { FaFacebook } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 
 const page = () => {
   return (
-    <div>
-      Signup Page
-    </div>
+    <div></div>
   )
 }
 
