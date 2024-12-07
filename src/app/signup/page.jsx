c9459cd4bc866c4537cd11f28dc71d8ee8fdce3b
shahriar2880 <shahriarreza436@gmail.com>
2024-12-07 21:16:52 +0600

    Create new user

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -7,8 +7,14 @@ import { FaFacebook } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
 
 const page = () => {
-  const handleLogin = async () =>{
-
+  const handleSignUp = async (event) =>{
+    event.preventDefault();
+    const newuser = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
+    }
+    console.log(newuser)
   }
   return (
     <div className='container px-24 mx-auto py-24'>
@@ -18,13 +24,13 @@ const page = () => {
         </div>
         <div className='border-2 p-12'>
         <h6 className='text-3xl font-semibold text-center mb-12'>Log In</h6>
-            <form onSubmit={handleLogin} action=''>
+            <form onSubmit={handleSignUp} action=''>
             <label htmlFor='name'>Name</label><br/>
             <input type="text" name='name' placeholder="Your Name" className="mt-3 input input-bordered w-full" /> <br/><br/>
             <label htmlFor='email'>Email</label><br/>
             <input type="text" name='email' placeholder="Your Email" className="mt-3 input input-bordered w-full" /> <br/><br/>
             <label htmlFor='password'>Password</label><br/>
-            <input type="text" name='password' placeholder="Your Password" className="mt-3 input input-bordered w-full" />
+            <input type="password" name='password' placeholder="Your Password" className="mt-3 input input-bordered w-full" />
             <br/><br/>
             <button type='submit' className='w-full btn btn-primary'>Sign Up</button>
             </form>
