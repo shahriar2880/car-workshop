85217448792eb70aea75d3587d58fc037dba6529
shahriar2880 <shahriarreza436@gmail.com>
2024-12-10 20:27:49 +0600

    create a provider in socialsignin

--- src/components/shared/SocialSignIn.jsx
+++ src/components/shared/SocialSignIn.jsx
@@ -4,15 +4,18 @@ import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 const SocialSignIn = () => {
+    const handleSocialLogIn = async (provider) => {
+
+    }
   return (
     <div className="flex items-center justify-center space-x-2">
-      <button className="btn flex items-center justify-center text-primary">
+      <button onClick={() => handleSocialLogIn('google')} className="btn flex items-center justify-center text-primary">
         <FcGoogle />
       </button>
-      <button className="btn flex items-center justify-center text-primary">
+      <button onClick={() => handleSocialLogIn('github')} className="btn flex items-center justify-center text-primary">
         <BsGithub />
       </button>
-      <button className="btn flex items-center justify-center text-primary">
+      <button onClick={() => handleSocialLogIn('facebook')} className="btn flex items-center justify-center text-primary">
         <FaFacebook />
       </button>
     </div>
