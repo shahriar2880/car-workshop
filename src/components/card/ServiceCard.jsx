2dabdc4de16dc37add28006e95ead2f680c69bb1
shahriar2880 <shahriarreza436@gmail.com>
2024-12-13 19:09:59 +0600

    show id when hover view details

--- src/components/card/ServiceCard.jsx
+++ src/components/card/ServiceCard.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ServiceCard = ({service}) => {
-    const {title, img,description,price} = service || {};
+    const {title, img,description,price, _id} = service || {};
   return (
     <div className="card card-compact bg-base-100 w-96 shadow-xl">
         <Image height={120} width={430} src={img} alt={title}/>
@@ -11,7 +12,8 @@ const ServiceCard = ({service}) => {
         <p>{description}</p>
         <div className="card-actions justify-between items-center">
             <h6 className="text-primary font-semibold">Price: ${price}</h6>
-          <button className="btn btn-primary">View More</button>
+            <Link href={`/services/${_id}`}><button className="btn btn-primary">View More</button></Link>
+          
         </div>
       </div>
     </div>
