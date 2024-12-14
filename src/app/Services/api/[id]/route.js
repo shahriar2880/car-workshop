bcf917882814536b757af45845812c5784ada088
shahriar2880 <shahriarreza436@gmail.com>
2024-12-14 20:57:27 +0600

    fetch single intem by api

---
+++ src/app/Services/api/[id]/route.js
@@ -0,0 +1,13 @@
+import { connectDB } from "@/lib/connectDB"
+
+export const GET = async (request, {params}) => {
+    const db = await connectDB()
+    const servicesCollection = db.collection('services')
+    // console.log(servicesCollection)
+    try {
+        const service = await servicesCollection.findOne({_id : params.id});
+        return Response.json({service})
+    } catch (error) {
+        console.log(error)
+    }
+}
